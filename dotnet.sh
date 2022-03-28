d090f2ed62340e79d8d582fafea6cfd2d2c1b685
Christian Sohns <christiansohns@gmx.net>
2022-03-28 16:57:22 +0200

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -196,8 +196,42 @@ public class DatabaseService
   }
 }
 " > Services/DatabaseService.cs
+echo "
+/*
+* Program.cs
+* Author: $author
+* Created on: $now
+*/
+using $name.Services;
 
-echo "Don't forget to run DatabaseService.Instance.SetupDatabase(); before executing app.Run();"
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+DatabaseService.Instance.DatabaseSetup();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+" > Program.cs
 
 # Open in VSCode
 code ../$name
